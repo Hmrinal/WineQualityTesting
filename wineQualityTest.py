b94727f5b3b5456ca298de4b9a54fa1a1054ea1c
Hmrinal <mrinaltyagi1912@gmail.com>
2020-08-22 22:20:52 +0530

    prediction commit

--- wineQualityTest.py
+++ wineQualityTest.py
@@ -28,3 +28,23 @@ features = get_features(0.05)
 print(features)
 x = df[features]
 y = df['quality']
+
+#dividing data into training & testing
+x_train,x_test,y_train,y_test = train_test_split(x,y,random_state=3)
+regressor = LinearRegression()
+regressor.fit(x_train,y_train)
+
+#predicting
+regressor.coef_
+train_pred = regressor.predict(x_train)
+train_pred
+test_pred=regressor.predict(x_test)
+test_pred
+
+#predicted data
+predicted_data = np.round_(test_pred)
+predicted_data
+coeffecients = pd.DataFrame(regressor.coef_,features)
+coeffecients.columns = ['Coeffecient']
+coeffecients
+
